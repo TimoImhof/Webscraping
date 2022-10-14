78fe71702f379e735dd0aeb3d0c01029d31424ef
Timo Imhof <timo.imhof.uni@gmail.com>
2022-10-14 09:39:20 +0200

    finish tutorial

--- V2.py
+++ V2.py
@@ -2,6 +2,8 @@ import urllib.request, sys, time
 from bs4 import BeautifulSoup
 import requests
 import pandas as pd
+import matplotlib.pyplot as plt
+
 
 # guide: https://towardsdatascience.com/scraping-1000s-of-news-articles-using-10-simple-steps-d57636a49755
 
@@ -30,11 +32,27 @@ except Exception as e:
 
 soup = BeautifulSoup(page.text, "html.parser")
 
+upperframe = []
+frame = []
+
 links = soup.find_all('li', attrs={'class': 'o-listicle__item'})
 for j in links:
     statement = j.find("div", attrs={'class': 'm-statement__quote'}).text.strip()
-    link = j.find("div", attrs={'class':'m-statement__quote'}).find('a')['href'].strip()
-    date = j.find('footer', attrs={'class':'m-statement__footer'}).text.strip()
-    print(statement)
-    print(link)
-    print('Date: '+date)
+    link = j.find("div", attrs={'class': 'm-statement__quote'}).find('a')['href'].strip()
+    date_and_author = j.find('footer', attrs={'class': 'm-statement__footer'}).text[-17:-1].strip()
+    title = j.find('div', attrs={'class': 'm-statement__author'}).find('a').get('title').strip()
+    frame.append([statement, link, date_and_author, title])
+    upperframe.extend(frame)
+
+#print(upperframe)
+
+data = pd.DataFrame(upperframe, columns=['statement', 'link', 'date_and_author', 'title'])
+print(data['title'])
+data.plot(x = 'statement', y = 'date_and_author', kind = 'scatter')
+plt.show()
+
+filename = "Example.csv"
+f = open(filename,'w')
+headers = 'Statement, Link, Date, Source, Label\n'
+f.write(headers)
+f.close()
