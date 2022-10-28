1b10308a18880ea6dab6329bf5edcd3560937d12
Timo Imhof <timo.imhof.uni@gmail.com>
2022-10-28 07:47:29 +0200

    add description

--- preprocessing.py
+++ preprocessing.py
@@ -1,2 +1,6 @@
+""" File to note for future work """
+
+""" Methods that need to be implemented:"""
 def eliminate_duplicates():
+    """ Might happen to extract duplicate articles when extracting from AP_News therefore this method"""
     pass
