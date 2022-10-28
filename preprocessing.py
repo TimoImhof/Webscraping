82009be40fbee0a1ad423ff4e9c510555b9d313d
Timo Imhof <timo.imhof.uni@gmail.com>
2022-10-28 07:49:18 +0200

    add description + add another future method

--- preprocessing.py
+++ preprocessing.py
@@ -1,6 +1,10 @@
-""" File to note for future work """
+""" File to note for future work for preprocessing """
 
 """ Methods that need to be implemented:"""
 def eliminate_duplicates():
     """ Might happen to extract duplicate articles when extracting from AP_News therefore this method"""
+    pass
+
+def combine_csv_files():
+    """ Need to combine all csv files """
     pass
