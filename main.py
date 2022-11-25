ecc4f7ed6a6cd723b90f5d707204e46ccf3a12bd
Timo Imhof <timo.imhof.uni@gmail.com>
2022-11-25 11:10:05 +0100

    add infinite loop as the scripts will be executed every 4 hours. Now ready for deployment in Docker Container

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import time
 import ap_news_scraping
 import tagesschau_scraping
 
-ap_news_scraping.start_retrieval()
-tagesschau_scraping.start_retrieval()
-#time.sleep(14400)  # execute script every 4 hours
+while True:
+    ap_news_scraping.start_retrieval()
+    tagesschau_scraping.start_retrieval()
+    time.sleep(14400)  # execute script every 4 hours
